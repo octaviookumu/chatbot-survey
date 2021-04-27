01a9590bb79bbbf59d1fe0cad99b726bb84574d1
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2021-04-28 01:45:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,44 @@ $(document).ready(function () {
         [], [], [], [], [], [], []
     ];
 
+    let question_options = [
+    {
+        question: "Are You A Youth? (Below 35 Years)",
+        question_number: 1,
+        options: ["Yes", "No"],
+    },
+    {
+        question: "What is your gender?",
+        question_number: 2,
+        options: ["Male", "Female"],
+    },
+    {
+        question: "Which county are you from?",
+        question_number: 3,
+        options: "",
+    },
+    {
+        question: "Which candidate would you want to vie?",
+        question_number: 4,
+        options: "",
+    },
+    {
+        question: "If elections were held today, which candidate would you want to vie?",
+        question_number: 5,
+        options: ["Raila Odinga", "William Ruto", "Fred Matiangi", "Musalia Mudavadi", "Kalonzo Musyoka", "Others"]
+    },
+    {
+        question: "If a referendum on the constitutional changes proposed in the BBI was held today, would you vote for or against the changes?",
+        question_number: 6,
+        options: ["For", "Against"]
+    },
+    {
+        question: "Do you support one man, one vote, one shilling policy?",
+        question_number: 7,
+        options: ["Yes", "No"]
+    }
+];
+
     $(window).load(function () {
         $messages.mCustomScrollbar();
         setTimeout(function () {
@@ -25,17 +63,17 @@ $(document).ready(function () {
     // Inserts message
     function insertMessage() {
         if (msgId === 7) return;
-        if (msgId !== 2 || msgId !== 3) clickedCandidate();
-        if (msgId === 2 || msgId === 3) {
+        if (msgId !== 3) clickedCandidate();
+        if (msgId === 3) {
             msg = $('.message-input').val();
             let text = msg.charAt(0).toUpperCase() + msg.slice(1).toLowerCase();
             if ($.trim(msg) == '') {
                 return false;
             }
             $('<div class="message message-personal">' + text + '</div>').appendTo($('.mCSB_container')).addClass('new');
-            if(msgId === 2) responses[msgId].push(text);
             if (msgId === 3) responses[msgId].push(text);
-            console.log(responses)
+            sendAdSurveyResponse(msgId + 1, text);
+            sendBannerEngagementEvent(`question_${msgId + 1}_input_entered`);
             $('.message-input').val(null);
             text = "";
         }
@@ -53,7 +91,7 @@ $(document).ready(function () {
             return false;
         }
         $('<div class="message message-personal">' + choice + '</div>').appendTo($('.mCSB_container')).addClass('new');
-       
+
         $('.message-input').val(null);
         updateScrollbar();
         setTimeout(function () {
@@ -63,12 +101,12 @@ $(document).ready(function () {
 
     // Inserts choice selected
     $('.message-submit').click(function () {
-        if(msgId === 2 || msgId === 3 ) insertMessage(); 
+        if (msgId === 3) insertMessage();
     });
 
     // Inserts typed text
     $(window).on('keydown', function (e) {
-        if (msgId === 2 || msgId === 3) {
+        if (msgId === 3) {
             if (e.which == 13) {
                 insertMessage();
                 return false;
@@ -97,6 +135,62 @@ $(document).ready(function () {
         <ul>
             <li class="option" data-value="Female">Female</li>
         </ul>
+    </div>`;
+
+    let questionThreeOptions = `
+    <div class="options question_2_options">
+        <div class="counties">
+        <select id="county-selected">
+            <option selected="selected" value="">Select county</option>
+            <option value="baringo">Baringo</option>
+            <option value="bomet">Bomet</option>
+            <option value="bungoma">Bungoma</option>
+            <option value="busia">Busia</option>
+            <option value="elgeyo marakwet">Elgeyo Marakwet</option>
+            <option value="embu">Embu</option>
+            <option value="garissa">Garissa</option>
+            <option value="homa bay">Homa Bay</option>
+            <option value="isiolo">Isiolo</option>
+            <option value="kajiado">Kajiado</option>
+            <option value="kakamega">Kakamega</option>
+            <option value="kericho">Kericho</option>
+            <option value="kiambu">Kiambu</option>
+            <option value="kilifi">Kilifi</option>
+            <option value="kirinyaga">Kirinyaga</option>
+            <option value="kisii">Kisii</option>
+            <option value="kisumu">Kisumu</option>
+            <option value="kitui">Kitui</option>
+            <option value="kwale">Kwale</option>
+            <option value="laikipia">Laikipia</option>
+            <option value="lamu">Lamu</option>
+            <option value="machakos">Machakos</option>
+            <option value="makueni">Makueni</option>
+            <option value="mandera">Mandera</option>
+            <option value="meru">Meru</option>
+            <option value="migori">Migori</option>
+            <option value="marsabit">Marsabit</option>
+            <option value="mombasa">Mombasa</option>
+            <option value="muranga">Muranga</option>
+            <option value="nairobi">Nairobi</option>
+            <option value="nakuru">Nakuru</option>
+            <option value="nandi">Nandi</option>
+            <option value="narok">Narok</option>
+            <option value="nyamira">Nyamira</option>
+            <option value="nyandarua">Nyandarua</option>
+            <option value="nyeri">Nyeri</option>
+            <option value="samburu">Samburu</option>
+            <option value="siaya">Siaya</option>
+            <option value="taita taveta">Taita Taveta</option>
+            <option value="tana river">Tana River</option>
+            <option value="tharaka nithi">Tharaka Nithi</option>
+            <option value="trans nzoia">Trans Nzoia</option>
+            <option value="turkana">Turkana</option>
+            <option value="uasin gishu">Uasin Gishu</option>
+            <option value="vihiga">Vihiga</option>
+            <option value="wajir">Wajir</option>
+            <option value="pokot">West Pokot</option>
+        </select>
+        </div>
     </div>`;
 
     let candidatesDiv = `
@@ -137,29 +231,38 @@ $(document).ready(function () {
     let choice = '';
 
     function clickedCandidate() {
-        // console.log(msgId)
-
-        if (msgId === 2 || msgId === 3) return;
-        if (msgId !== 2 || msgId !== 3) {
+        if (msgId === 3) return;
+        if (msgId === 2) {
+            document.querySelector('#county-selected').addEventListener('change', (e) => {
+                let county = e.target.value;
+                let county_selected = county.charAt(0).toUpperCase() + county.slice(1).toLowerCase();
+                insertChoiceMade(county_selected);
+                responses[2].push(county_selected);
+                document.querySelector('.question_2_options').style.display = 'none';
+                sendAdSurveyResponse(msgId + 1, county_selected);
+                sendBannerEngagementEvent(`question_${msgId + 1}_option_${county}_selected`);
+                msgId++;
+            });
+        }
+        if (msgId !== 2 || msgId !== 3 ) {
             let questionOptions = document.querySelector(`.question_${msgId}_options`);
             let options = questionOptions.querySelectorAll('.option');
-            
 
             options.forEach((option, i) => {
                 option.addEventListener('click', () => {
                     choice = options[i].getAttribute("data-value");
+                    let first_word = choice.split(" ")[0];
                     insertChoiceMade(choice);
                     questionOptions.style.display = 'none';
-                    // return choice;
                     responses[msgId].push(choice);
-                    console.log(responses)
+                    sendAdSurveyResponse(msgId + 1, choice);
+                    sendBannerEngagementEvent(`question_${msgId + 1}_option_${first_word}_selected`);
                     choice = '';
                     msgId++;
                 });
             });
         }
     }
-
 
     var Fake = [
         {
@@ -171,8 +274,8 @@ $(document).ready(function () {
             options: questionTwoOptions
         },
         {
-            question: 'Which country are you from?',
-            options: ''
+            question: 'Which county are you from?',
+            options: questionThreeOptions
         },
         {
             question: 'Who would you like to see vying for the next Presidential Elections?',
@@ -198,24 +301,56 @@ $(document).ready(function () {
         if ($('.message-input').val() != '') {
             return false;
         }
-        $('<div class="message loading new"><figure class="avatar"><img src="https://cdn.diversityavatars.com/assets/images/avatars-gallery/placeholder.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+        $('<div class="message loading new"><figure class="avatar"><img src="images/chatbot-avatar.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
         updateScrollbar();
 
         setTimeout(function () {
             $('.message.loading').remove();
             if (i === 7) {
-                $('<div class="message new"><figure class="avatar"><img src="https://cdn.diversityavatars.com/assets/images/avatars-gallery/placeholder.png" /></figure>' + 'Thank you for completing the survey.' + '</div>').appendTo($('.mCSB_container')).addClass('new');
+                $('<div class="message new"><figure class="avatar"><img src="images/chatbot-avatar.png" /></figure>' + 'Thank you for completing the survey.' + '</div>').appendTo($('.mCSB_container')).addClass('new');
                 updateScrollbar();
-                document.querySelector('.message-box').style.display = 'none';
+                // document.querySelector('.message-box').style.display = 'none';
+                // console.log(responses)
                 return;
             } else {
-                $('<div class="message new"><figure class="avatar"><img src="https://cdn.diversityavatars.com/assets/images/avatars-gallery/placeholder.png" /></figure>' + Fake[i].question + '<br>' + Fake[i].options + '</div>').appendTo($('.mCSB_container')).addClass('new');
+                $('<div class="message new"><figure class="avatar"><img src="images/chatbot-avatar.png" /></figure>' + Fake[i].question + '<br>' + Fake[i].options + '</div>').appendTo($('.mCSB_container')).addClass('new');
                 clickedCandidate();
             }
             updateScrollbar();
             i++;
         }, 1000 + (Math.random() * 20) * 100);
     }
+
+
+
+
+    function sendAdSurveyResponse(question_number, response) {
+    //  console.log(unique_id);
+
+    let data = {
+        "banner_id": creative_id,
+        "banner_name": options.banner_name,
+        "impression_unique_id": unique_id,
+        ...question_options[question_number - 1],
+        response: [response]
+    };
+
+    //   console.log(data);
+
+    (async () => {
+        const rawResponse = await fetch('https://dxp.mediapal.net/api/leadgen/adsurvey/create/1234567890', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        //   const content = await rawResponse.json();
+
+        //   console.log(content);
+    })();
+}
 });
 
 
